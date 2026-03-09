329a70a2e8c68393fafc82bc77cec2c884aa4db1
SebasL07 <sebasgnv0207@gmail.com>
2026-03-09 16:09:39 -0500

    refactor: there is no creation of users now

--- src/modules/iam/pages/UsersPage.tsx
+++ src/modules/iam/pages/UsersPage.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@mui/material";
-import { useNavigate } from "react-router-dom"
 import CustomTitle from "../../../shared/components/Title/Title";
 import UsersContent from "../components/UsersContent/UsersContent";
 import { Button } from "../../../shared/components/Button/Button";
@@ -9,7 +8,6 @@ import { Plus } from "lucide-react";
 const UsersPage = () => {
     
     const breadcrumbs = [{label: 'Usuarios'}]
-    const navigate = useNavigate();
 
     return (
         <Box className="flex h-full flex-col space-y-6">
@@ -18,10 +16,9 @@ const UsersPage = () => {
                 <Box className="flex items-center justify-end">
                     <Button
                         variant="primary"
-                        onClick={() => {navigate("/usuarios/crear")}}
                     >
                         <Plus className="w-4 h-4 mr-2" />
-                        Crear usuario
+                        Invitar usuario
                     </Button>
                 </Box>
             </Box>
